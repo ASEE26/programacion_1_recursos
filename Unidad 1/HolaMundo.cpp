0e17dd83218d83cdb3644529a6749164603b15d3
= <pabloesau26@gmail.com>
2022-01-28 15:42:08 -0600

    Resumen
    Resumen

---
+++ Unidad 1/HolaMundo.cpp
@@ -0,0 +1,10 @@
+// Un lenguaje de programación es el medio por el cual nos podemos "Comunicar" fácilmente con un ordenador
+// Sin embargo, es necesario respetar las reglas que componen a este lenguaje de programación, estas reglas es llamada SINTAXIS
+// de un lenguaje de programación. El lenguaje que usaremos es conocimo como c++ o lenguaje de los puntos y comas.
+// Por último, la traducción del lenguaje de programación al maquina se hace mediante un COMPILADOR
+
+//SINTAXIS básica y general en c++:
+// 1. Incluir o incorporar librerias
+// 2. Definir la función  principal
+// 3. escribir las instrucciones a ejecutar.
+
